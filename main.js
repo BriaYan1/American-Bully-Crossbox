83083b71e307641f356b91fe63b7a238280af57c
Briayan <briayang1997@gmail.com>
2023-05-23 21:11:18 -0400

    Version-0.1-Menu-Hamburguesa

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Menu Hamburguesa*/
+
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const cerrar = document.querySelector("#cerrar");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible");
+    })    
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+
