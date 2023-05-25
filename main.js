63180b02a43b22b10870d0ca307bacd6fea5921a
Briayan <briayang1997@gmail.com>
2023-05-24 21:57:16 -0400

    v-0.0.4-Burguer

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /*Menu Hamburguesa*/
-
+    /*Esto no sirve, no hace nada*/
     const nav = document.querySelector("#nav");
     const abrir = document.querySelector("#abrir");
     const cerrar = document.querySelector("#cerrar");
+    /*Esto no sirve, no hace nada*/
 
     abrir.addEventListener("click", () => {
         nav.classList.add("visible");
