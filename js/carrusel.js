1eaca5a9738eb8777ca3c8733ff709378864f993
Alejandro Mory <aalejandromory@gmail.com>
2023-07-21 20:53:24 -0400

    V 0.13.2 MASTER(Arreglado bug carrusel)

--- js/carrusel.js
+++ js/carrusel.js
@@ -6,47 +6,36 @@ let imagenes = [
     "assets/competitions/americangames/american5.jpg"
 ];
 
-document.Imagen.src = imagenes[0]; //Al cargar la pagina muestra la imagen ubicada en el espacio 0 del arreglo.
-
-let sliderDerecho = document.querySelector(".slider-derecho"); //Toma el elemento con el id slider-derecho.
+document.Imagen.src = imagenes[0];
+let sliderDerecho = document.querySelector(".slider-derecho");
 let sliderIzquierdo = document.querySelector(".slider-izquierdo");
-let cont=0;
+let cont = 0;
+let intervalo = setInterval(MoverDerecha, 4000);
 
 function MoverDerecha() {
     cont++;
-
-    if(cont > imagenes.length-1) {
-        cont=0;
+    if (cont > imagenes.length - 1) {
+        cont = 0;
     }
-
     document.Imagen.src = imagenes[cont];
 }
-
-let intervalo = setInterval(MoverDerecha, 4000);
-sliderDerecho.addEventListener("click", function(){
-    clearInterval(intervalo);
-    MoverDerecha();
-    intervalo = setInterval(MoverDerecha, 4000);
-})
-
-sliderDerecho.addEventListener("click", MoverDerecha); //Ejecuta la funcion MoverDerecha al hacer click.
-
-
 
 function MoverIzquierda() {
     cont--;
-
-    if(cont < 0) {
-        cont=imagenes.length-1;
+    if (cont < 0) {
+        cont = imagenes.length - 1;
     }
-    
     document.Imagen.src = imagenes[cont];
 }
 
-sliderIzquierdo.addEventListener("click", function(){
+sliderDerecho.addEventListener("click", function() {
+    clearInterval(intervalo);
+    MoverDerecha();
+    intervalo = setInterval(MoverDerecha, 4000);
+});
+
+sliderIzquierdo.addEventListener("click", function() {
     clearInterval(intervalo);
     MoverIzquierda();
-    intervalo = setInterval(MoverIzquierda, 4000);
-})
-
-sliderIzquierdo.addEventListener("click", MoverIzquierda);
+    intervalo = setInterval(MoverDerecha, 4000);
+});
