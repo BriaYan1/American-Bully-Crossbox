1dba6660d219e104fb665beb6516bdc803f7a058
Alejandro Mory <aalejandromory@gmail.com>
2023-07-23 21:51:06 -0400

    V0.14 MASTER (Agregado PopUp)

---
+++ js/modales.js
@@ -0,0 +1,95 @@
+const modal1 = document.getElementById('modal--1');
+const abrirModal1 = document.querySelector('#abrir-modal--1');
+const cerrarModal1 = document.getElementById('cerrar-modal--1');
+
+abrirModal1.addEventListener("click", ()=>{
+    modal1.showModal();
+})
+
+cerrarModal1.addEventListener("click", ()=>{
+    modal1.close();
+})
+
+const modal2 = document.getElementById('modal--2');
+const abrirModal2 = document.querySelector('#abrir-modal--2');
+const cerrarModal2 = document.getElementById('cerrar-modal--2');
+
+abrirModal2.addEventListener("click", ()=>{
+    modal2.showModal();
+})
+
+cerrarModal2.addEventListener("click", ()=>{
+    modal2.close();
+})
+
+const modal3 = document.getElementById('modal--3');
+const abrirModal3 = document.querySelector('#abrir-modal--3');
+const cerrarModal3 = document.getElementById('cerrar-modal--3');
+
+abrirModal3.addEventListener("click", ()=>{
+    modal3.showModal();
+})
+
+cerrarModal3.addEventListener("click", ()=>{
+    modal3.close();
+})
+
+const modal4 = document.getElementById('modal--4');
+const abrirModal4 = document.querySelector('#abrir-modal--4');
+const cerrarModal4 = document.getElementById('cerrar-modal--4');
+
+abrirModal4.addEventListener("click", ()=>{
+    modal4.showModal();
+})
+
+cerrarModal4.addEventListener("click", ()=>{
+    modal4.close();
+})
+
+const modal5 = document.getElementById('modal--5');
+const abrirModal5 = document.querySelector('#abrir-modal--5');
+const cerrarModal5 = document.getElementById('cerrar-modal--5');
+
+abrirModal5.addEventListener("click", ()=>{
+    modal5.showModal();
+})
+
+cerrarModal5.addEventListener("click", ()=>{
+    modal5.close();
+})
+
+const modal6 = document.getElementById('modal--6');
+const abrirModal6 = document.querySelector('#abrir-modal--6');
+const cerrarModal6 = document.getElementById('cerrar-modal--6');
+
+abrirModal6.addEventListener("click", ()=>{
+    modal6.showModal();
+})
+
+cerrarModal6.addEventListener("click", ()=>{
+    modal6.close();
+})
+
+const modal7 = document.getElementById('modal--7');
+const abrirModal7 = document.querySelector('#abrir-modal--7');
+const cerrarModal7 = document.getElementById('cerrar-modal--7');
+
+abrirModal7.addEventListener("click", ()=>{
+    modal7.showModal();
+})
+
+cerrarModal7.addEventListener("click", ()=>{
+    modal7.close();
+})
+
+const modal8 = document.getElementById('modal--8');
+const abrirModal8 = document.querySelector('#abrir-modal--8');
+const cerrarModal8 = document.getElementById('cerrar-modal--8');
+
+abrirModal8.addEventListener("click", ()=>{
+    modal8.showModal();
+})
+
+cerrarModal8.addEventListener("click", ()=>{
+    modal8.close();
+})
